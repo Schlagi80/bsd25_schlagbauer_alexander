3d6b46ff09efa0e52515943f1026e9fb34963b2f
Alexander Schlagbauer <52504170@campus02.at>
2026-06-24 18:10:58 +0200

    Basic Login zur Main Klasse hinzugefügt

--- src/main/java/at/campus02/bsd/Main.java
+++ src/main/java/at/campus02/bsd/Main.java
@@ -1,8 +1,19 @@
 package at.campus02.bsd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+
+    public static final Logger logger = LogManager.getLogger(Main.class);
+
     static void main(String[] args) {
         System.out.println("Alexander Schlagbauer");
-    }
 
+
+
+        logger.info("Eine Info-Nachricht zum Testen.");
+        logger.error("Eine Error-Nachricht zum Testen.");
+
+    }
 }
